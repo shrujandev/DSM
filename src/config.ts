da537cb840ff3d67e39cbd3dcdd4767eb000dc5a
Shrinithi Natarajan <kavinattu2001@gmail.com>
2023-05-01 22:50:15 +0530

    removed cors problem

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
     ethUrl: process.env.REACT_APP_ETH_URL || "http://127.0.0.1:7545",
-    ipfsApiUrl: process.env.REACT_APP_IPFS_API_URL || "http://127.0.0.1:5001/api/v0",
+    ipfsApiUrl: process.env.REACT_APP_IPFS_API_URL || "http://127.0.0.1:5002/api/v0",
 };
 
 export default config;
